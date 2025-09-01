78385a053a017e888d5da5cd6582a516126f085c
callum mccafrey <callummccaffrey2025@gmail.com>
2025-09-02 09:36:24 +1000

    chore: set outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
 };
+
 export default nextConfig;
