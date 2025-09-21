7e563779b8ec2d73b843c7263d464127922b8849
callum mccafrey <callummccaffrey2025@gmail.com>
2025-09-21 18:50:33 +1000

    fix(ui): add input.tsx and normalize UI imports to lowercase

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
+  { className, ...props }, ref
+) {
+  return (
+    <input
+      ref={ref}
+      className={[
+        "w-full rounded-lg border border-zinc-700 bg-zinc-900/40 px-3 py-2 text-sm",
+        "outline-none focus:ring-2 focus:ring-white/30",
+        className
+      ].filter(Boolean).join(" ")}
+      {...props}
+    />
+  );
+});
+export default Input;
